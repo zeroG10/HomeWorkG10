e93d55e661a57ac31ded7deb43c5bfa0115cca3e
zeroG10 <nick.smoock@gmail.com>
2020-09-09 00:17:16 +0300

    Rename file. Homework - program code by Lists.

--- HW_List.py
+++ HW_List.py
@@ -1,6 +1,6 @@
 different_types = [55, 58.4, 'hi', 479, 55, 'zero', 2]
 
 different_types.insert(4, 'spider')
-different_types.pop(-1)
+different_types.pop( -1 )  # or different_types.pop()
 
 print(different_types)
